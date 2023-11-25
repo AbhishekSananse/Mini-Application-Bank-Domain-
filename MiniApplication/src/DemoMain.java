d857379c582919f4c132a351d8d651e9c9e72e74
Abhishek Sananse <76435686+AbhishekSananse@users.noreply.github.com>
2023-11-25 11:54:02 +0530

    Add files via upload

---
+++ MiniApplication/src/DemoMain.java
@@ -0,0 +1,11 @@
+import DepositMoney.Deposit;
+import Home.Homepage;
+
+public class DemoMain {
+	public static void main(String[]args) { 
+	Homepage hp = new Homepage();
+	hp.home();
+//	Deposit d =new Deposit();
+//	d.DepAmt(1140);
+	}
+}
